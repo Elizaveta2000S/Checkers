9d9e64326907a761e2160b0a6fbbb53bbcbf9e6a
Natalia2000Bush <62169930+Natalia2000Bush@users.noreply.github.com>
2020-04-04 14:42:05 +0300

    проверка

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1.1());
+            Application.Run(new Form1());
         }
     }
 }
